b2bb31688e6f2faae482630f5d484f2ed4ee5774
methiathibault <methiathibault@gmail.com>
2024-06-04 17:06:01 +0200

    fixing conflicts

--- src/components/RouterComponents.jsx
+++ src/components/RouterComponents.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Products from './Products'
 import Product from './Product'
+import Login from './Login'
+import Testing from './Testing'
 
 export default function RouterComponents() {
     return (
         <Routes>
             <Route path='/' element={<Products />} />
             <Route path='/product/:idProduit' element={<Product />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/testing' element={<Testing />} />
             {/* <Route path='/Cart' element={<Cart />} />
             <Route path='/Login' element={<Login />} /> */}
         </Routes>
